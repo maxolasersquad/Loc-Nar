1d1dfc0df630886bfdb564bad3cc88f09d802d1d
David Baucum <david@baucum.me>
2026-06-04 19:45:07 -0400

    Added matcha package

---
+++ matcha/description.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+printf 'A beautiful, functional, and secure \e[38;5;150mterminal email client\e[0m built in Go using the Bubble Tea TUI framework. Supports IMAP/JMAP, PGP signing, HTML rendering, and custom themes.\n'
